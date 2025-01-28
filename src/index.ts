9bb5955f8ebb0e90ba68e424ee75d3ec5db9e39f
Matt <47070087+Camo651@users.noreply.github.com>
2025-01-28 14:25:28 -0500

    sockets

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,27 @@
 import 'dotenv/config'
 import { initApp } from './app';
+import {initSockets} from './socket';
 
 const start = async () => {
-    const app = await initApp();
-    const port = process.env.API_PORT;
     const url = process.env.API_URL;
-    if (!port || !url) {
-        throw new Error('API_PORT and API_URL must be provided');
+    const API_PORT = process.env.API_PORT;
+    const LOW_BAND_SOCKET_PORT = process.env.LOW_BAND_SOCKET_PORT;
+    const HIGH_BAND_SOCKET_PORT = process.env.HIGH_BAND_SOCKET_PORT;
+    if (!API_PORT || !url || !LOW_BAND_SOCKET_PORT || !HIGH_BAND_SOCKET_PORT) {
+        throw new Error('Make sure to set all required environment variables');
     }
 
-    app.listen(port, () => {
-        console.log(`Server started at ${url}:${port}`);
+    const app = await initApp();
+    app.listen(API_PORT, () => {
+        console.log(`Server started at ${url}:${API_PORT}`);
+    });
+
+    const { lowBandServer, highBandServer } = initSockets();
+    lowBandServer.listen(LOW_BAND_SOCKET_PORT, () => {
+        console.log('Low Band Socket Server started at port ', LOW_BAND_SOCKET_PORT);
+    });
+    highBandServer.listen(HIGH_BAND_SOCKET_PORT, () => {
+        console.log('High Band Socket Server started at port ', HIGH_BAND_SOCKET_PORT);
     });
 }
 
