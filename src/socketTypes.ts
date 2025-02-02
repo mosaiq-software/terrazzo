7f16f529c226c85b4dde47c5361bb2406a253ea1
Matt <47070087+Camo651@users.noreply.github.com>
2025-02-01 20:32:12 -0500

    socket types

--- src/socketTypes.ts
+++ src/socketTypes.ts
@@ -1,3 +1,5 @@
+import { Board } from "./types";
+
 export enum ClientSocketIOEvent {
     CONNECT = "connect",
     DISCONNECT = "disconnect",
@@ -10,34 +12,79 @@ export enum ServerSocketIOEvent {
     DISCONNECTING = 'disconnecting',
 }
 
-export enum SocketEventTypes {
-    INITIALIZE = "INITIALIZE",
-    CLIENT_CONNECT = "CLIENT_CONNECT",
-    CLIENT_DISCONNECT = "CLIENT_DISCONNECT",
-    CHANGE_ROOM = "CHANGE_ROOM",
-    MOUSE_MOVE = "MOUSE_MOVE",
-    USER_IDLE = "USER_IDLE",
+// SE = Socket Event
+export enum ClientSE { // Client to Server
+    SET_ROOM = "C_SET_ROOM",
+    MOUSE_MOVE = "C_MOUSE_MOVE",
+    USER_IDLE = "C_USER_IDLE",
+    GET_BOARD = "C_GET_BOARD",
 }
-export interface UserData {
-    sid: string;
+export enum ServerSE { // Server to Client
+    CLIENT_JOINED_ROOM = "S_CLIENT_JOINED_ROOM",
+    CLIENT_LEFT_ROOM = "S_CLIENT_LEFT_ROOM",
+    MOUSE_MOVE = "S_MOUSE_MOVE",
+    USER_IDLE = "S_USER_IDLE",
+}
+
+export interface SEPayload {
+    // Client to Server
+    [ClientSE.SET_ROOM]: RoomId;
+    [ClientSE.MOUSE_MOVE]: Position;
+    [ClientSE.USER_IDLE]: boolean;
+    [ClientSE.GET_BOARD]: string;
+
+    // Server to Client
+    [ServerSE.CLIENT_JOINED_ROOM]: UserData;
+    [ServerSE.CLIENT_LEFT_ROOM]: SocketId;
+    [ServerSE.MOUSE_MOVE]: { sid: SocketId; data: MouseUserData };
+    [ServerSE.USER_IDLE]: { sid: SocketId; idle: boolean };
+
+}
+
+// types for the callback functions for each event type
+export interface SEReplies {
+    // Client to Server req - Server to Client callback
+    [ClientSE.SET_ROOM]: { users: UserData[] };
+    [ClientSE.MOUSE_MOVE]: undefined;
+    [ClientSE.USER_IDLE]: undefined;
+    [ClientSE.GET_BOARD]: { board: Board };
+
+    // Server to Client req - Client to Server callback
+    [ServerSE.CLIENT_JOINED_ROOM]: undefined;
+    [ServerSE.CLIENT_LEFT_ROOM]: undefined;
+    [ServerSE.MOUSE_MOVE]: undefined;
+    [ServerSE.USER_IDLE]: undefined;
+}
+export interface SEReply<T extends keyof SEReplies> {
+    type: T;
+    data: SEReplies[T];
+    error?: string;
+}
+
+export interface MouseUserData extends Position {};
+
+export interface TextUserData {
+    cursor: number;
+    highlight: number;
+}
+export interface UserGeneralData {
     githubId: string;
     username: string;
     avatarUrl: string;
     fullName: string;
     idle: boolean;
-    mouse: {
-        x?: number;
-        y?: number;
-    }
 }
-export interface SocketEventPayload {
-    [SocketEventTypes.INITIALIZE]: { roomUsers: UserData[]; };
-    [SocketEventTypes.CLIENT_CONNECT]: UserData;
-    [SocketEventTypes.CLIENT_DISCONNECT]: { sid: string, room: string };
-    [SocketEventTypes.CHANGE_ROOM]: string;
-    [SocketEventTypes.MOUSE_MOVE]: { sid: string, x: number, y: number };
-    [SocketEventTypes.USER_IDLE]: { sid: string, idle: boolean };
+export interface UserData extends UserGeneralData {
+    sid: SocketId;
+    data: MouseUserData | TextUserData;
 }
+
+export type Position = { x: number; y: number; }
+
+export type RoomId = string | null;
+export type SocketId = string;
+
+
 export interface SocketData {
     connectedAt: Date;
     access_token: string;
