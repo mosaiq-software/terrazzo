ebe7990d9c9c9f26fa1be78645d1575efac8a8c6
Matt <47070087+Camo651@users.noreply.github.com>
2025-02-07 12:52:13 -0500

    text replace

--- src/controllers/textBlockController.ts
+++ src/controllers/textBlockController.ts
@@ -1,6 +1,26 @@
 import { TextBlockEvent } from "@mosaiq/terrazzo-common/types"
+import { getTextBlockById } from "@trz-api/persistence/textBlockPersistence";
 
 
 export const handleTextBlockEvent = async (event: TextBlockEvent) => {
-    // get the text block,
+    let {id, start, end, inserted} = event;
+    
+    // Get the text block
+    const textBlock = await getTextBlockById(id);
+    if(!textBlock){
+        throw new Error("Text block not found")
+    }
+
+    // Clean start and end positions
+    if(start > end){
+        [start, end] = [end, start];
+    }
+    start = Math.max(0, start);
+    end = Math.min(end, textBlock.text.length);
+    console.log("start:",start,"end:",end);
+    console.log(textBlock.text.substring(0, start));
+    console.log(inserted);
+    console.log(textBlock.text.substring(end));
+    const newText = textBlock.text.substring(0, start) + inserted + textBlock.text.substring(end);
+    console.log(newText);
 }
