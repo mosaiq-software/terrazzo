b51011d099dc0fc8414da26069cf81e466cf8589
Matt <47070087+Camo651@users.noreply.github.com>
2025-02-08 00:07:40 -0500

    translation on exec tbe

--- src/utils/textUtils.ts
+++ src/utils/textUtils.ts
@@ -1,9 +1,32 @@
+import { Position } from '../socketTypes';
 import {TextBlockEvent} from '../types';
 
-export const executeTextBlockEvent = (textBlock: string, event: TextBlockEvent) => {
+export const executeTextBlockEvent = (textBlock: string, event: TextBlockEvent, selectionStart?: number): {updated:string, selectionStart: number} => {
     let {start, end, inserted} = event;
     if(start > end){ [start, end] = [end, start]; }
-    return textBlock.substring(0, Math.max(0, start)) + inserted + textBlock.substring(Math.min(end, textBlock.length));
+
+    start = Math.max(0, start);
+    end = Math.min(end, textBlock.length);
+
+    const updated = textBlock.substring(0, start) + inserted + textBlock.substring(end);
+    if (!selectionStart){
+        return {updated, selectionStart: 0};
+    }
+
+    let newSelStart = 0;
+    if (selectionStart < start) {
+        // before changed area, leave alone
+        newSelStart = selectionStart;
+    } else if (selectionStart < end) {
+        // inside deleted area, move to where deletion started
+        newSelStart = start;
+    } else {
+        // after deleted area, shift back by deletion then forwards by insertion
+        const deleteDelta = end - start;
+        newSelStart = (selectionStart - deleteDelta) + inserted.length;
+    }
+
+    return {updated, selectionStart:newSelStart}
 }
 
 export const isValidTextBlockEvent = (event?:TextBlockEvent) => {
