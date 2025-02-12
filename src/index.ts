6f2dc58f620f55f59a6233ea538e14d6442d3d0e
Matt <47070087+Camo651@users.noreply.github.com>
2025-02-12 10:29:16 -0500

    updated env vars

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,24 @@ import { initApp } from './app';
 import {initSockets} from './utils/socket';
 
 const start = async () => {
-    const url = process.env.API_URL;
-    const API_PORT = process.env.API_PORT;
     const SOCKET_PORT = parseInt(process.env.SOCKET_PORT+'') || undefined;
-    if (!API_PORT || !url || !SOCKET_PORT) {
+    if (
+        !SOCKET_PORT ||
+        !process.env.API_PORT ||
+        !process.env.API_URL ||
+        !process.env.GITHUB_AUTH_CLIENT_SECRET ||
+        !process.env.GITHUB_AUTH_CALLBACK_URL ||
+        !process.env.GITHUB_AUTH_CLIENT_ID ||
+        !process.env.ORG_NAME ||
+        !process.env.DATABASE_PATH ||
+        !process.env.DATABASE_LOGGING
+    ) {
         throw new Error('Make sure to set all required environment variables');
     }
 
     const app = await initApp();
-    app.listen(API_PORT, () => {
-        console.log(`Server started at ${url}:${API_PORT}`);
+    app.listen(process.env.API_PORT, () => {
+        console.log(`Server started at ${process.env.API_URL}:${process.env.API_PORT}`);
     });
 
     const { io } = initSockets();
