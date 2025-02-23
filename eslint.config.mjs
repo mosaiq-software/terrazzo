71f10e3afb2716c33159fbb31c48035faa7c646e
Matt <47070087+Camo651@users.noreply.github.com>
2025-02-23 16:39:14 -0500

    node types

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,10 +6,9 @@ import tseslint from "typescript-eslint";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
     {files: ["**/*.{js,mjs,cjs,ts}"]},
-    {languageOptions: { globals: globals.browser }},
+    {languageOptions: { globals: {...globals.browser, ...globals.node} }},
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
-    { languageOptions: { globals: globals.browser } },
     {
         rules: {
             eqeqeq: "off",
