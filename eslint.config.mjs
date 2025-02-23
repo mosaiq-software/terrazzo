9390a3b068f4a8a36e36203dab10b6e89b872c41
Matt <47070087+Camo651@users.noreply.github.com>
2025-02-23 16:36:57 -0500

    added eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+    {files: ["**/*.{js,mjs,cjs,ts}"]},
+    {languageOptions: { globals: globals.browser }},
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    { languageOptions: { globals: globals.browser } },
+    {
+        rules: {
+            eqeqeq: "off",
+            "no-unused-vars": "off",
+            "prefer-const": ["error", { ignoreReadBeforeAssign: true }],
+        },
+    },
+    {
+        ignores: [".node_modules/*", "dist/*", "scripts/*"]
+    },
+];
