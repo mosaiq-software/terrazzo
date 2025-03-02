7d672ca4f8960ddf4422bf2542188fff035cc537
Matt <47070087+Camo651@users.noreply.github.com>
2025-03-01 23:28:02 -0500

    added api typing

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as types from './types';
 import * as socketTypes from './socketTypes';
+import * as apiTypes from './apiTypes';
 import * as constants from './constants';
 import * as textUtils from './utils/textUtils';
 import * as socketUtils from './utils/socketUtils';
@@ -8,6 +9,7 @@ import * as arrayUtils from './utils/arrayUtils';
 export { 
     types,
     socketTypes,
+    apiTypes,
     constants,
     textUtils,
     socketUtils,
