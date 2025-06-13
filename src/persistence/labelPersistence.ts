1425abf4c5689b161943369af5c4c069ffed93ba
Matt <47070087+Camo651@users.noreply.github.com>
2025-06-12 23:05:20 -0400

    Label system

--- src/persistence/labelPersistence.ts
+++ src/persistence/labelPersistence.ts
@@ -1,6 +1,6 @@
 import { Model, DataTypes } from 'sequelize';
 import { sequelize } from './dbHelper';
-import { BoardId, Label, LabelId } from '@mosaiq/terrazzo-common/types';
+import { BoardId, CardId, Label, LabelId } from '@mosaiq/terrazzo-common/types';
 
 class LabelModel extends Model {}
 LabelModel.init({
@@ -12,6 +12,16 @@ LabelModel.init({
     name: DataTypes.STRING,
     color: DataTypes.STRING
 }, { sequelize, modelName: 'labelModel' });
+
+class LabeledCardModel extends Model {}
+LabeledCardModel.init({
+    id: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+    },
+    labelId: DataTypes.STRING,
+    cardId: DataTypes.STRING,
+}, { sequelize, modelName: 'labeledCardModel' });
 
 sequelize.sync();
 
@@ -45,4 +55,27 @@ export const deleteLabel = async (id: LabelId) => {
 
 export const deleteLabelsByBoardId = async (boardId: BoardId) => {
     return await LabelModel.destroy({ where: { boardId } });
+}
+
+
+
+
+export const getLabelsOnCard = async (cardId:CardId) => {
+    return (await LabeledCardModel.findAll({ where: { cardId } })).map(label => label.toJSON().labelId) as LabelId[];
+}
+
+export const deleteLabelingOnCardsByLabelId = async (labelId:LabelId) => {
+    return await LabeledCardModel.destroy({ where: { labelId } });
+}
+
+export const deleteLabelsOnCard = async (cardId:CardId) => {
+    return await LabeledCardModel.destroy({ where: { cardId } });
+}
+
+export const addLabelToCard = async (labelId:LabelId, cardId:CardId) => {
+    return await LabeledCardModel.create({
+        id: crypto.randomUUID(),
+        cardId,
+        labelId
+    });
 }
