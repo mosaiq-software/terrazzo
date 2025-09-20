f6b5b4ebd8550c09cb7c3a82d7c02aa6c0660609
Matt <47070087+Camo651@users.noreply.github.com>
2025-09-20 18:25:43 -0400

    docker

--- Dockerfile
+++ Dockerfile
@@ -8,8 +8,4 @@ WORKDIR /app
 
 RUN npm install
 
-EXPOSE 3001
-EXPOSE 3002
-EXPOSE 3003
-
 CMD ["npm", "run", "start"]
--- src/utils/dbHelper.ts
+++ src/utils/dbHelper.ts
@@ -1,7 +1,7 @@
 import {Sequelize} from 'sequelize';
 export const sequelize = new Sequelize({
     dialect: 'sqlite',
-    storage: process.env.DATABASE_PATH ?? "./undefinedDatabase.sqlite",
+    storage: `${process.env.DATABASE_PATH}/trz.sqlite`,
     logging: (process.env.DATABASE_LOGGING === "true")
 });
 
