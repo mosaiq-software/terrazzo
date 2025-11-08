10a1abbf852a5960d5b8f62e02aa56bc354feac4
Matt <47070087+Camo651@users.noreply.github.com>
2025-11-08 10:41:37 -0500

    basic vite setup

--- terrazzo-ui/eslint.config.mjs
+++ terrazzo-ui/eslint.config.mjs
@@ -1,17 +1,24 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
-
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-    {files: ["**/*.{ts,tsx}"]},
-    {languageOptions: { globals: globals.browser }},
-    pluginJs.configs.recommended,
-    ...tseslint.configs.recommended,
-    pluginReact.configs.flat.recommended,
+export default defineConfig([
+    globalIgnores(['dist']),
     {
+        files: ['**/*.{ts,tsx}'],
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommended,
+            reactHooks.configs['recommended-latest'],
+            reactRefresh.configs.vite,
+        ],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
         rules: {
             eqeqeq: "off",
             "@typescript-eslint/no-unused-vars": "off",
@@ -23,7 +30,4 @@ export default [
             "react/jsx-pascal-case": "warn",
         }
     },
-    {
-        ignores: [".node_modules/*", "dist/*", "scripts/*", "*.cjs", "public/*"]
-    },
-];
+])
