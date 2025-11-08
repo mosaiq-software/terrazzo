b46438ece3c0c7067e591435645e2ec0f13ec7ab
Matt <47070087+Camo651@users.noreply.github.com>
2025-11-07 22:38:03 -0500

    cleanup

--- terrazzo-ui/src/emitters/inviteEmitters.ts
+++ terrazzo-ui/src/emitters/inviteEmitters.ts
@@ -1,8 +1,7 @@
 import { EntityType, Role } from '@mosaiq/terrazzo-common/constants';
 import { ClientSE } from '@mosaiq/terrazzo-common/socketTypes';
-import { Board, BoardHeader, BoardId, BoardRes, Card, CardHeader, CardId, DocumentHeader, DocumentId, EntityId, Invite, InviteId, Label, LabelId, List, ListHeader, ListId, MembershipRecord, MembershipRecordId, Organization, OrganizationHeader, OrganizationId, Project, ProjectHeader, ProjectId, UID, UserDash, UserId } from '@mosaiq/terrazzo-common/types';
+import { MembershipRecordId, MembershipRecord, EntityId, InviteId } from '@mosaiq/terrazzo-common/types';
 import { SocketContextType } from '@trz/contexts/socket-context';
-import { NoteType, notify } from '@trz/util/notifications';
 
 export const updateMembershipRecordField = async (sockCtx: SocketContextType, id: MembershipRecordId, partial: Partial<MembershipRecord>) => {
     await sockCtx.emit(ClientSE.UPDATE_MEMBERSHIP_RECORD_FIELD, { ...partial, id });
--- terrazzo-ui/src/emitters/userEmitters.ts
+++ terrazzo-ui/src/emitters/userEmitters.ts
@@ -1,3 +1,8 @@
+import { ClientSE } from '@mosaiq/terrazzo-common/socketTypes';
+import { UserId, UserDash } from '@mosaiq/terrazzo-common/types';
+import { SocketContextType } from '@trz/contexts/socket-context';
+import { notify, NoteType } from '@trz/util/notifications';
+
 export const getUsersDash = async (sockCtx: SocketContextType, userId: UserId): Promise<UserDash | undefined> => {
     try {
         const response = await sockCtx.emit<ClientSE.GET_USER_DASH>(ClientSE.GET_USER_DASH, userId);
