459daa5e69f9709f21f90a6e51dabdc7697347fa
Matt <47070087+Camo651@users.noreply.github.com>
2025-11-08 11:50:43 -0500

    svg typedef

--- terrazzo-ui/src/types/svg.d.ts
+++ terrazzo-ui/src/types/svg.d.ts
@@ -1,3 +1,5 @@
+/// <reference types="vite/client" />
+/// <reference types="vite-plugin-svgr/client" />
 declare module '*.svg' {
     import React from 'react';
     const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
