63519c2bbf4a894b04205baa0334b4330fd1c68b
Matt <47070087+Camo651@users.noreply.github.com>
2025-12-06 10:18:28 -0500

    perm group types

--- terrazzo-common/src/types.ts
+++ terrazzo-common/src/types.ts
@@ -202,25 +202,40 @@ export enum PermissionFlag {
     DUMMY_2 = 'DUMMY_2',
     DUMMY_3 = 'DUMMY_3',
 }
+export enum PermissionFlagCategory {
+    DUMMY = 'DUMMY',
+}
 export interface PermissionFlagData {
     flag: PermissionFlag;
     title: string;
     description: string;
+    category: PermissionFlagCategory;
 }
 export const PermissionFlagData: Record<PermissionFlag, PermissionFlagData> = {
     [PermissionFlag.DUMMY_1]: {
         flag: PermissionFlag.DUMMY_1,
         title: 'Dummy Permission 1',
         description: 'This is a dummy permission for testing purposes.',
+        category: PermissionFlagCategory.DUMMY,
     },
     [PermissionFlag.DUMMY_2]: {
         flag: PermissionFlag.DUMMY_2,
         title: 'Dummy Permission 2',
         description: 'This is another dummy permission for testing purposes.',
+        category: PermissionFlagCategory.DUMMY,
     },
     [PermissionFlag.DUMMY_3]: {
         flag: PermissionFlag.DUMMY_3,
         title: 'Dummy Permission 3',
         description: 'This is yet another dummy permission for testing purposes.',
+        category: PermissionFlagCategory.DUMMY,
+    },
+};
+export interface PermissionFlagCategoryData {
+    title: string;
+}
+export const PermissionFlagCategoryData: Record<PermissionFlagCategory, PermissionFlagCategoryData> = {
+    [PermissionFlagCategory.DUMMY]: {
+        title: 'Dummy Permissions',
     },
 };
