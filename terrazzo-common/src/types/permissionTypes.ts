3096a5a0f41efc9c5db6d85aaf2fd729f002ce23
Matt <47070087+Camo651@users.noreply.github.com>
2025-12-07 14:51:31 -0500

    fg perms

--- terrazzo-common/src/types/permissionTypes.ts
+++ terrazzo-common/src/types/permissionTypes.ts
@@ -18,19 +18,23 @@ export enum PermissionFlag {
     EDIT_ROLES = 'EDIT_ROLES',
     ASSIGN_ROLES = 'ASSIGN_ROLES',
 
-    // Module level permissions
-    VIEW_MODULE = 'VIEW_MODULE',
-    EDIT_MODULE = 'EDIT_MODULE',
-
     // Board permissions
+    VIEW_BOARD = 'VIEW_BOARD',
+    EDIT_BOARD = 'EDIT_BOARD',
+    CREATE_BOARD = 'CREATE_BOARD',
     MOVE_CARDS = 'MOVE_CARDS',
     EDIT_CARDS = 'EDIT_CARDS',
+    CREATE_CARDS = 'CREATE_CARDS',
+
+    // Document permissions
+    VIEW_DOCUMENT = 'VIEW_DOCUMENT',
+    EDIT_DOCUMENT = 'EDIT_DOCUMENT',
+    CREATE_DOCUMENT = 'CREATE_DOCUMENT',
 }
 export enum PermissionFlagCategory {
     ORG_LEVEL = 'ORG_LEVEL',
-    MODULE_LEVEL = 'MODULE_LEVEL',
     BOARD_LEVEL = 'BOARD_LEVEL',
-
+    DOCUMENT_LEVEL = 'DOCUMENT_LEVEL',
     OTHER = 'OTHER',
 }
 export interface PermissionFlagData {
@@ -49,8 +53,8 @@ export const PermissionFlagData: Record<PermissionFlag, PermissionFlagData> = {
     },
     [PermissionFlag.EDIT_ROLES]: {
         flag: PermissionFlag.EDIT_ROLES,
-        title: 'Edit Roles',
-        description: 'Ability to create and edit roles within the organization',
+        title: 'Manage Roles',
+        description: 'Ability to create and edit roles and their permissions',
         category: PermissionFlagCategory.ORG_LEVEL,
     },
     [PermissionFlag.ASSIGN_ROLES]: {
@@ -60,21 +64,25 @@ export const PermissionFlagData: Record<PermissionFlag, PermissionFlagData> = {
         category: PermissionFlagCategory.ORG_LEVEL,
     },
 
-    // Module level permissions
-    [PermissionFlag.VIEW_MODULE]: {
-        flag: PermissionFlag.VIEW_MODULE,
-        title: 'View Module',
-        description: 'Ability to view boards, documents, etc.',
-        category: PermissionFlagCategory.MODULE_LEVEL,
-    },
-    [PermissionFlag.EDIT_MODULE]: {
-        flag: PermissionFlag.EDIT_MODULE,
-        title: 'Edit Module',
-        description: 'Ability to create and edit boards, documents, etc.',
-        category: PermissionFlagCategory.MODULE_LEVEL,
-    },
-
     // Board permissions
+    [PermissionFlag.VIEW_BOARD]: {
+        flag: PermissionFlag.VIEW_BOARD,
+        title: 'View Board',
+        description: 'Ability to view a board and its contents',
+        category: PermissionFlagCategory.BOARD_LEVEL,
+    },
+    [PermissionFlag.EDIT_BOARD]: {
+        flag: PermissionFlag.EDIT_BOARD,
+        title: 'Edit Board',
+        description: 'Ability to edit a board and its lists',
+        category: PermissionFlagCategory.BOARD_LEVEL,
+    },
+    [PermissionFlag.CREATE_BOARD]: {
+        flag: PermissionFlag.CREATE_BOARD,
+        title: 'Create Board',
+        description: 'Ability to create new boards',
+        category: PermissionFlagCategory.BOARD_LEVEL,
+    },
     [PermissionFlag.MOVE_CARDS]: {
         flag: PermissionFlag.MOVE_CARDS,
         title: 'Move Cards',
@@ -87,6 +95,32 @@ export const PermissionFlagData: Record<PermissionFlag, PermissionFlagData> = {
         description: 'Ability to edit any field of a card',
         category: PermissionFlagCategory.BOARD_LEVEL,
     },
+    [PermissionFlag.CREATE_CARDS]: {
+        flag: PermissionFlag.CREATE_CARDS,
+        title: 'Create Cards',
+        description: 'Ability to create new cards on a board',
+        category: PermissionFlagCategory.BOARD_LEVEL,
+    },
+
+    // Document permissions
+    [PermissionFlag.VIEW_DOCUMENT]: {
+        flag: PermissionFlag.VIEW_DOCUMENT,
+        title: 'View Document',
+        description: 'Ability to view a document',
+        category: PermissionFlagCategory.DOCUMENT_LEVEL,
+    },
+    [PermissionFlag.EDIT_DOCUMENT]: {
+        flag: PermissionFlag.EDIT_DOCUMENT,
+        title: 'Edit Document',
+        description: 'Ability to edit a document',
+        category: PermissionFlagCategory.DOCUMENT_LEVEL,
+    },
+    [PermissionFlag.CREATE_DOCUMENT]: {
+        flag: PermissionFlag.CREATE_DOCUMENT,
+        title: 'Create Document',
+        description: 'Ability to create new documents',
+        category: PermissionFlagCategory.DOCUMENT_LEVEL,
+    },
 };
 export interface PermissionFlagCategoryData {
     title: string;
@@ -95,13 +129,13 @@ export const PermissionFlagCategoryData: Record<PermissionFlagCategory, Permissi
     [PermissionFlagCategory.ORG_LEVEL]: {
         title: 'Organization Permissions',
     },
-    [PermissionFlagCategory.MODULE_LEVEL]: {
-        title: 'Any Module Permissions',
-    },
     [PermissionFlagCategory.BOARD_LEVEL]: {
         title: 'Board Permissions',
     },
+    [PermissionFlagCategory.DOCUMENT_LEVEL]: {
+        title: 'Document Permissions',
+    },
     [PermissionFlagCategory.OTHER]: {
-        title: 'Other',
+        title: 'Miscellaneous Permissions',
     },
 };
