aa4574cd0d5eb70f63f9da1e1c7f91d8f745e68a
Matt <47070087+Camo651@users.noreply.github.com>
2025-12-19 14:34:46 -0500

    user settings

--- terrazzo-ui/src/contexts/unsaved-changes-context.tsx
+++ terrazzo-ui/src/contexts/unsaved-changes-context.tsx
@@ -5,6 +5,7 @@ import React, { createContext, useCallback, useContext, useState } from 'react';
 export enum Savable {
     RoleSettings = 'role settings',
     OrgSettings = 'organization settings',
+    UserSettings = 'user settings',
 }
 
 export type UnsavedChangesContextType = {
--- terrazzo-ui/src/pages/UserSettingsPage.tsx
+++ terrazzo-ui/src/pages/UserSettingsPage.tsx
@@ -1,14 +1,61 @@
-import { Box, Fieldset, Group, ScrollArea, Space, Stack, Title } from '@mantine/core';
-import { fullName } from '@mosaiq/terrazzo-common';
+import { Box, Button, Fieldset, Group, ScrollArea, Stack, Title } from '@mantine/core';
+import { fullName, UserHeader } from '@mosaiq/terrazzo-common';
+import EditableTextbox from '@trz/components/UI/EditableTextbox';
+import { ImageUpload } from '@trz/components/UI/ImageUpload';
 import { NotFound, PageErrors } from '@trz/components/UI/NotFound';
+import { useSocket } from '@trz/contexts/socket-context';
 import { useUI } from '@trz/contexts/ui-context';
+import { Savable, useUnsavedChanges } from '@trz/contexts/unsaved-changes-context';
+import { updateUserField } from '@trz/emitters';
 import { useMe } from '@trz/hooks/useMe';
+import { NoteType, notify } from '@trz/util/notifications';
 import { setTitle } from '@trz/util/tabUtils';
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 
 const UserSettingsPage = (): React.JSX.Element => {
     const uiCtx = useUI();
     const me = useMe();
+    const sockCtx = useSocket();
+    const unsavedCtx = useUnsavedChanges();
+
+    const [editedUserData, setEditedUserData] = useState<Partial<UserHeader>>({});
+
+    const change = useCallback(<K extends keyof UserHeader>(field: K, value: UserHeader[K]) => {
+        setEditedUserData((prev) => ({
+            ...prev,
+            [field]: value,
+        }));
+    }, []);
+
+    const isSaved = useMemo(() => {
+        let saved = true;
+        Object.keys(editedUserData).forEach((key) => {
+            const k = key as keyof UserHeader;
+            if (editedUserData[k] !== undefined && me && editedUserData[k] !== me[k]) {
+                saved = false;
+            }
+        });
+        return saved;
+    }, [editedUserData, me]);
+
+    useEffect(() => {
+        unsavedCtx.setSavedState(Savable.UserSettings, isSaved);
+    }, [isSaved]);
+
+    const handleSave = useCallback(
+        async (changes: Partial<UserHeader>) => {
+            try {
+                if (!me) {
+                    throw new Error('No user data available');
+                }
+                await updateUserField(sockCtx, { ...changes, id: me.id });
+                notify(NoteType.CHANGES_SAVED);
+            } catch (e) {
+                notify(NoteType.GENERIC_ERROR, e);
+            }
+        },
+        [me, sockCtx]
+    );
 
     useEffect(() => {
         setTitle(`My Settings | Terrazzo`);
@@ -52,75 +99,56 @@ const UserSettingsPage = (): React.JSX.Element => {
                             bg="transparent"
                         >
                             <Stack>
-                                {/* User settings */}
-                                {/* <TextInput
-                                    labelProps={{
-                                        c: 'white',
+                                <EditableTextbox
+                                    value={editedUserData.username ?? me.username}
+                                    onChange={(val) => change('username', val)}
+                                    placeholder="Username"
+                                    type="title"
+                                    titleProps={{
+                                        order: 3,
                                     }}
-                                    label="Board Name"
-                                    placeholder="My Board"
-                                    required
-                                    value={boardData.name ?? ''}
-                                    onChange={(e) => {
-                                        setBoardData({ ...boardData, name: e.target.value });
-                                        setIsDirty(true);
-                                    }}
+                                    style={{ flex: 1 }}
                                 />
-                                <TextInput
-                                    w="8rem"
-                                    labelProps={{
-                                        c: 'white',
-                                    }}
-                                    label="Board Code"
-                                    placeholder=""
-                                    value={boardData.boardCode ?? ''}
-                                    onChange={(e) => {
-                                        setBoardData({ ...boardData, boardCode: e.target.value });
-                                        setIsDirty(true);
-                                    }}
-                                />
+                                <Group>
+                                    <EditableTextbox
+                                        value={editedUserData.firstName ?? me.firstName}
+                                        onChange={(val) => change('firstName', val)}
+                                        placeholder="First Name"
+                                        style={{ flex: 1 }}
+                                    />
+                                    <EditableTextbox
+                                        value={editedUserData.lastName ?? me.lastName}
+                                        onChange={(val) => change('lastName', val)}
+                                        placeholder="Last Name"
+                                        style={{ flex: 1 }}
+                                    />
+                                </Group>
+                                <Group>
+                                    <ImageUpload
+                                        currentImageUrl={editedUserData.profilePicture ?? me.profilePicture}
+                                        onUploadComplete={(url) => handleSave({ profilePicture: url })}
+                                        width={100}
+                                        height={100}
+                                        alt="Profile Picture"
+                                    />
+                                    <EditableTextbox
+                                        value={editedUserData.profilePicture ?? me.profilePicture}
+                                        onChange={(val) => change('profilePicture', val)}
+                                        placeholder="Profile Picture URL"
+                                        style={{ flex: 1 }}
+                                    />
+                                </Group>
                                 <Button
-                                    disabled={!isDirty}
-                                    variant="filled"
-                                    onClick={async () => {
-                                        try {
-                                            updateBoardField(sockCtx, boardId, boardData);
-                                            notify(NoteType.CHANGES_SAVED);
-                                            setIsDirty(false);
-                                        } catch (e) {
-                                            notify(NoteType.BOARD_DATA_ERROR, e);
-                                        }
+                                    disabled={isSaved}
+                                    onClick={() => {
+                                        handleSave(editedUserData);
+                                        setEditedUserData({});
                                     }}
                                 >
                                     Save
-                                </Button> */}
+                                </Button>
                             </Stack>
                         </Fieldset>
-                        {/* <Divider /> */}
-                        <Space />
-                        {/* <Text>Joined {new Date(userCtx.userData.joinedAt).toLocaleString()}</Text> */}
-                        {/* <Text>Board contains {boardData.totalCards} cards</Text> */}
-                        {/* <Divider /> */}
-                        <Space />
-                        <Group gap="sm">
-                            {/* Account deletion one day... */}
-                            {/* <Button
-                                variant="light"
-                                color="red"
-                                w="min-content"
-                                onClick={() => {
-                                    try {
-                                        updateBoardField(sockCtx, boardId, { archived: true });
-                                        notify(NoteType.CHANGES_SAVED);
-                                        navigate(`/project/${boardData.projectId}`);
-                                    } catch (e) {
-                                        notify(NoteType.BOARD_DATA_ERROR, e);
-                                    }
-                                }}
-                            >
-                                Archive Board
-                            </Button> */}
-                        </Group>
                     </Stack>
                 </Box>
             </Stack>
