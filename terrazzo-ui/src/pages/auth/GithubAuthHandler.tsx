263b534b451e1985f3176cb8061f1ae7b3c400be
Matt <47070087+Camo651@users.noreply.github.com>
2025-12-21 14:02:15 -0500

    Handle deny login process

--- terrazzo-ui/src/pages/auth/GithubAuthHandler.tsx
+++ terrazzo-ui/src/pages/auth/GithubAuthHandler.tsx
@@ -1,15 +1,29 @@
-import { Center, Container, Loader, Paper, Stack, Text, Title } from '@mantine/core';
+import { Button, Center, Container, Loader, Paper, Stack, Text, Title } from '@mantine/core';
 import { AuthProvider } from '@mosaiq/terrazzo-common';
 import { useUserContext } from '@trz/contexts/user-context';
 import { useEffect } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+
+const USER_DENIED_APPLICATION = {
+    error: 'access_denied',
+    errorDescription: 'The user has denied your application access.',
+};
 
 /*
  * This page will only show as the callback from GitHub OAuth flow
  */
 export const GithubAuthHandler = () => {
     const userContext = useUserContext();
-    const code = useSearchParams()[0].get('code');
+    const navigate = useNavigate();
+
+    const [searchParams] = useSearchParams();
+    const code = searchParams.get('code');
+    const error = searchParams.get('error');
+    const errorDescription = searchParams.get('error_description');
+
+    // ?error=access_denied
+    // &error_description=The+user+has+denied+your+application+access.
+    // &error_uri=https%3A%2F%2Fdocs.github.com%2Fapps%2Fmanaging-oauth-apps%2Ftroubleshooting-authorization-request-errors%2F%23access-denied
 
     useEffect(() => {
         const handleLogin = () => {
@@ -29,6 +43,17 @@ export const GithubAuthHandler = () => {
         return () => clearTimeout(timeoutId);
     }, [code, userContext.userId, userContext.authToken]);
 
+    useEffect(() => {
+        if (error === USER_DENIED_APPLICATION.error && errorDescription === USER_DENIED_APPLICATION.errorDescription) {
+            console.warn('User denied application access during GitHub OAuth flow');
+            navigate('/login');
+            return;
+        } else if (error) {
+            console.error(`Error during GitHub OAuth flow: ${error} - ${errorDescription}`);
+            return;
+        }
+    }, [error, errorDescription]);
+
     return (
         <Container
             h="100%"
@@ -47,12 +72,28 @@ export const GithubAuthHandler = () => {
                         c={'#ebebeb'}
                         align="center"
                     >
-                        <Title>Working on it...</Title>
-                        <Text>Hold tight! We&apos;re logging you in with GitHub.</Text>
-                        <Loader
-                            type="bars"
-                            size="xl"
-                        />
+                        {error ? (
+                            <>
+                                <Title>Error during GitHub login</Title>
+                                <Text>{error}</Text>
+                                {errorDescription && <Text>{errorDescription}</Text>}
+                                <Button
+                                    mt={20}
+                                    onClick={() => navigate('/login')}
+                                >
+                                    Back to Login
+                                </Button>
+                            </>
+                        ) : (
+                            <>
+                                <Title>Working on it...</Title>
+                                <Text>Hold tight! We&apos;re logging you in with GitHub.</Text>
+                                <Loader
+                                    type="bars"
+                                    size="xl"
+                                />
+                            </>
+                        )}
                     </Stack>
                 </Paper>
             </Center>
