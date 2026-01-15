495b2bab36e2bdbcf03d556c98c763d45d2cdb30
Matt <47070087+Camo651@users.noreply.github.com>
2026-01-14 23:00:15 -0500

    Refactor queryController to enhance data retrieval for users in an organization and improve indexing of boards and cards

--- terrazzo-api/src/controllers/queryController.ts
+++ terrazzo-api/src/controllers/queryController.ts
@@ -1,12 +1,29 @@
-import { OrganizationId, QueryableDatapoint, QueryResult, UserId } from '@mosaiq/terrazzo-common';
+import {
+    boardNameWithCode,
+    CardHeader,
+    cardNameWithBoardCodeAndNumber,
+    ModuleHeader,
+    OrganizationId,
+    QueryableDatapoint,
+    QueryableItem,
+    QueryResult,
+    settlePromises,
+    TrzModuleType,
+    UserId,
+} from '@mosaiq/terrazzo-common';
+import { getBoardByIdDb } from '@trz-api/persistence/boardPersistence';
+import { getCardsByBoardIdDb } from '@trz-api/persistence/cardPersistence';
+import { getDocumentByIdDb } from '@trz-api/persistence/documentPersistence';
+import { getModulesByOrgIdDb } from '@trz-api/persistence/modulePersistence';
 import { getOrganizationMembershipsForUserDb } from '@trz-api/persistence/organizationMembershipPersistence';
 import { getOrgByIdDb } from '@trz-api/persistence/organizationPersistence';
 import Fuse from 'fuse.js';
+import { getQueryableTextBlockContent } from './textBlockController';
 
 /** Cache each search session so that we only index once per use of the searchbar */
 const CachedSearchSessions = new Map<UserId, { searchSessionId: string; datapoints: QueryableDatapoint[] }>();
 
-const getAllQueryableDataForUser = async (userId: UserId) => {
+const getAllQueryableDataForUserInOrg = async (userId: UserId, orgId: OrganizationId) => {
     const orgMemberships = await getOrganizationMembershipsForUserDb(userId);
     const allOrgIds = new Set<OrganizationId>();
     for (const om of orgMemberships) {
@@ -17,86 +34,108 @@ const getAllQueryableDataForUser = async (userId: UserId) => {
 
     const queryableData: QueryableDatapoint[] = [];
 
-    // Board and cards
-    //TODO make this use the new dir structure
-    // const allProjectIds = new Set<UID>();
-    // for (const pid of allProjectIds) {
-    //     const boardIds = await getBoardsByParentId(pid);
-    //     for (const board of boardIds) {
-    //         if (!board || board.archived) continue;
-    //         queryableData.push({
-    //             title: board.name,
-    //             display: `[${board.boardCode}] ${board.name}`,
-    //             content: `[${board.boardCode}] ${board.name}`.toLowerCase(),
-    //             id: board.id,
-    //             type: DatapointType.BoardTitle,
-    //         });
-    //         const lists = await getListsByBoardId(board.id);
-    //         for (const list of lists) {
-    //             if (list.archived) continue;
-    //             const cards = await getCardsByListId(list.id);
-    //             for (const card of cards) {
-    //                 if (card.archived) continue;
-    //                 queryableData.push({
-    //                     title: card.name,
-    //                     display: `[${board.boardCode.length ? `${board.boardCode}-` : ''}${card.cardNumber}] ${card.name}`,
-    //                     content: `${board.boardCode.length ? `${board.boardCode}-` : ''}${card.cardNumber} ${card.name}`.toLowerCase(),
-    //                     id: card.id,
-    //                     type: DatapointType.CardTitle,
-    //                 });
-    //                 const textBlockId = card.descriptionTextBlockId;
-    //                 if (!textBlockId) continue;
-    //                 const cardDescriptionEncoded = await getTextBlockById(textBlockId);
-    //                 if (!cardDescriptionEncoded) continue;
-    //                 const decodedText = remirrorYjsToPlaintext(cardDescriptionEncoded.text);
-    //                 if (decodedText.trim().length === 0) continue;
-    //                 queryableData.push({
-    //                     title: card.name,
-    //                     display: decodedText,
-    //                     content: decodedText.toLowerCase(),
-    //                     id: card.id,
-    //                     type: DatapointType.CardDescription,
-    //                 });
-    //             }
-    //         }
-    //     }
-    // }
+    const org = await getOrgByIdDb(orgId);
+    if (!org) {
+        return queryableData;
+    }
 
-    // // Documents
-    // const parentIds = new Set<UID>([...allProjectIds, ...allOrgIds]);
-    // for (const parentId of parentIds) {
-    //     const documents = await getAllDocumentsForParent(parentId);
-    //     for (const doc of documents) {
-    //         if (doc.archived) continue;
-    //         const title = doc.title;
-    //         queryableData.push({
-    //             title: title,
-    //             display: title,
-    //             content: title.toLowerCase(),
-    //             id: doc.id,
-    //             type: DatapointType.DocumentTitle,
-    //         });
-    //         const textBlock = await getTextBlockById(doc.textBlockId);
-    //         if (!textBlock) continue;
-    //         const content = remirrorYjsToPlaintext(textBlock.text);
-    //         queryableData.push({
-    //             title: title,
-    //             display: content,
-    //             content: content.toLowerCase(),
-    //             id: doc.id,
-    //             type: DatapointType.DocumentContent,
-    //         });
-    //     }
-    // }
+    const allBoardsInOrg = await getModulesByOrgIdDb(org.id, { type: TrzModuleType.Board, archived: false });
+    const boardIndexingPromises = allBoardsInOrg.map((boardModule) => indexBoard(boardModule));
 
+    const allDocumentsInOrg = await getModulesByOrgIdDb(org.id, { type: TrzModuleType.Document, archived: false });
+    const documentIndexingPromises = allDocumentsInOrg.map((documentModule) => indexDocument(documentModule));
+
+    const allIndexingPromises = [...boardIndexingPromises, ...documentIndexingPromises];
+    const { fulfilled } = await settlePromises(allIndexingPromises);
+    for (const result of fulfilled) {
+        queryableData.push(...result);
+    }
     return queryableData;
 };
 
-export const executeQueryForUser = async (userId: UserId, query: string, searchSessionId: string) => {
+const indexDocument = async (documentModule: ModuleHeader): Promise<QueryableDatapoint[]> => {
+    try {
+        const document = await getDocumentByIdDb(documentModule.id);
+        if (!document) {
+            throw new Error('Document not found');
+        }
+        const textBlockContent = await getQueryableTextBlockContent(document.textBlockId);
+        const documentQueryable: QueryableDatapoint = {
+            display: documentModule.name,
+            content: `${documentModule.name} ${textBlockContent}`.toLowerCase(),
+            id: document.id,
+            type: QueryableItem.Document,
+        };
+        return [documentQueryable];
+    } catch (e) {
+        console.error('Error indexing document for query:', {
+            documentId: documentModule.id,
+            error: e,
+        });
+        throw e;
+    }
+};
+
+const indexBoard = async (boardModule: ModuleHeader): Promise<QueryableDatapoint[]> => {
+    try {
+        const board = await getBoardByIdDb(boardModule.id);
+        if (!board) {
+            throw new Error('Board not found');
+        }
+        const boardQueryable: QueryableDatapoint = {
+            display: boardNameWithCode(boardModule.name, board.boardCode),
+            content: boardNameWithCode(boardModule.name, board.boardCode).toLowerCase(),
+            id: board.id,
+            type: QueryableItem.Board,
+        };
+
+        const cardsInBoard = await getCardsByBoardIdDb(boardModule.id, { archived: false });
+        const cardIndexingPromises: Promise<QueryableDatapoint>[] = [];
+        for (const card of cardsInBoard) {
+            cardIndexingPromises.push(indexCard(card, board.boardCode));
+        }
+        const { fulfilled } = await settlePromises(cardIndexingPromises);
+
+        return [boardQueryable, ...fulfilled];
+    } catch (e) {
+        console.error('Error indexing board for query:', {
+            boardId: boardModule.id,
+            error: e,
+        });
+        throw e;
+    }
+};
+
+const indexCard = async (card: CardHeader, boardCode: string): Promise<QueryableDatapoint> => {
+    try {
+        const textBlockContent = await getQueryableTextBlockContent(card.descriptionTextBlockId);
+        const cardQueryable: QueryableDatapoint = {
+            display: cardNameWithBoardCodeAndNumber(card.name, boardCode, card.cardNumber),
+            content:
+                `${cardNameWithBoardCodeAndNumber(card.name, boardCode, card.cardNumber)} ${textBlockContent}`.toLowerCase(),
+            id: card.id,
+            type: QueryableItem.Card,
+        };
+        return cardQueryable;
+    } catch (e) {
+        console.error('Error indexing card for query:', {
+            cardId: card.id,
+            error: e,
+        });
+        throw e;
+    }
+};
+
+export const executeQueryForUser = async (
+    userId: UserId,
+    orgId: OrganizationId,
+    query: string,
+    searchSessionId: string
+) => {
     let queryableData: QueryableDatapoint[] = [];
     const cachedSession = CachedSearchSessions.get(userId);
     if (!cachedSession || cachedSession.searchSessionId !== searchSessionId) {
-        queryableData = await getAllQueryableDataForUser(userId);
+        queryableData = await getAllQueryableDataForUserInOrg(userId, orgId);
         CachedSearchSessions.set(userId, { searchSessionId, datapoints: queryableData });
     } else {
         queryableData = cachedSession.datapoints;
