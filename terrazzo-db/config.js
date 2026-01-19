8375a48091dc95c808e69283b32cbdee6fd4f41e
Matt <47070087+Camo651@users.noreply.github.com>
2026-01-19 07:21:57 -0500

    set up database package

---
+++ terrazzo-db/config.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const dotenv = require('dotenv');
+dotenv.config({ path: '../.env' });
+
+const dbDir = process.env.DATABASE_DIR;
+const dbName = process.env.DATABASE_NAME;
+const dbLogging = process.env.DATABASE_LOGGING === 'true';
+
+if(!dbDir) {
+    throw new Error('DATABASE_DIR environment variable is not set.');
+}
+
+if(!dbName) {
+    throw new Error('DATABASE_NAME environment variable is not set.');
+}
+
+const sqliteStorage = `${dbDir}/${dbName}`;
+
+module.exports = {
+    development: {
+        dialect: 'sqlite',
+        storage: sqliteStorage,
+        logging: dbLogging,
+        dialectOptions: {},
+    },
+};
