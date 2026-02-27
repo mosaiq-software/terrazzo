a3f35374f2f4fa1c4ca75c839b38804fbaf3694e
Matt <47070087+Camo651@users.noreply.github.com>
2026-02-27 00:22:42 -0500

    collection handler types

--- terrazzo-common/src/types/dataSource/collectionSources.ts
+++ terrazzo-common/src/types/dataSource/collectionSources.ts
@@ -1,17 +1,68 @@
-import { LabelId } from '../genericTypes';
+import { CardId, InviteId, LabelId, ListId, ModuleId, RoleId, TextBlockSnapshotId, UserId } from '../genericTypes';
 
 export enum CollectionSource {
+    // Read only sources
+    Cards = 'Cards',
+    Lists = 'Lists',
     Labels = 'Labels',
+    Invites = 'Invites',
+    Modules = 'Modules',
+    Roles = 'Roles',
+    TextBlockSnapshots = 'TextBlockSnapshots',
+
+    // Editable sources
+    CardAssignees = 'CardAssignees',
     LabelAssignments = 'LabelAssignments',
+    OrganizationMembers = 'OrganizationMembers',
+    RoleAssignments = 'RoleAssignments',
 }
 
 export interface CollectionSourceDataMap {
-    [CollectionSource.LabelAssignments]: {
-        of: LabelId;
-        editable: true;
+    // Read only sources
+    [CollectionSource.Cards]: {
+        of: CardId;
+        editable: false;
+    };
+    [CollectionSource.Lists]: {
+        of: ListId;
+        editable: false;
     };
     [CollectionSource.Labels]: {
         of: LabelId;
         editable: false;
+    };
+    [CollectionSource.Invites]: {
+        of: InviteId;
+        editable: false;
+    };
+    [CollectionSource.Modules]: {
+        of: ModuleId;
+        editable: false;
+    };
+    [CollectionSource.Roles]: {
+        of: RoleId;
+        editable: false;
+    };
+    [CollectionSource.TextBlockSnapshots]: {
+        of: TextBlockSnapshotId;
+        editable: false;
+    };
+
+    // Editable sources
+    [CollectionSource.CardAssignees]: {
+        of: CardId;
+        editable: true;
+    };
+    [CollectionSource.LabelAssignments]: {
+        of: LabelId;
+        editable: true;
+    };
+    [CollectionSource.OrganizationMembers]: {
+        of: UserId;
+        editable: true;
+    };
+    [CollectionSource.RoleAssignments]: {
+        of: RoleId;
+        editable: true;
     };
 }
